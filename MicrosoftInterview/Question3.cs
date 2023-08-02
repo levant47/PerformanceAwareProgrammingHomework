e0547d8e295af1f086beae190aca0ff4c86baf1c
levant47 <levant.ba@gmail.com>
2023-08-03 02:18:54 +0300

    Question 3 broken :(

--- MicrosoftInterview/Question3.cs
+++ MicrosoftInterview/Question3.cs
@@ -1,20 +1,25 @@
 ﻿public class Question3Tests
 {
     [Test]
-    [TestCase(0b10, 0b10, true)]
-    [TestCase(0b01010110, 0b10, true)]
-    [TestCase(0b10_11_11_11, 0b10, true)]
-    [TestCase(0b11_01_11_01, 0b10, false)]
-    [TestCase(0, 0, true)]
-    [TestCase(0b00101101, 0, true)]
-    public void ContainsColorWorks(byte pixel, byte color, bool expected) => Assert.AreEqual(expected, Question3.ContainsColor(pixel, color));
+    public void ContainsColorWorks()
+    {
+        var allColors = new byte[] { 0b00, 0b01, 0b10, 0b11 };
+        var allBytes = Enumerable.Range(0, 256).Select(n => (byte)n).ToList();
+        var allInputs = allColors.SelectMany(color => allBytes.Select(pixel => new { Pixel = pixel, Color = color })).ToList();
+        CollectionAssert.AreEqual(
+            allInputs.Select(input => new { input.Pixel, input.Color, Result = Question3.ContainsColorTrivial(input.Pixel, input.Color) }),
+            allInputs.Select(input => new { input.Pixel, input.Color, Result = Question3.ContainsColor(input.Pixel, input.Color) })
+        );
+    }
 }
 
 public static class Question3
 {
-    // I don't think this version is really any faster than the loop one (assuming the compiler unrolls the loop in the)
+    // I don't think this version is really any faster than the loop one (assuming the compiler unrolls the loop in the loop version)
     public static bool ContainsColor(byte pixel, byte color)
     {
+        // TODO: this doesn't work!!!
+
         // pack the color into a byte with 4 pixels of the same color
         var color4 = color | (color << 2) | (color << 4) | (color << 6);
         // compute the difference between all 4 pairs of pixels
@@ -25,8 +30,7 @@ public static class Question3
         return (difference & oddBits) != oddBits && (difference & evenBits) != evenBits;
     }
 
-    /* first version:
-    public static bool ContainsColor(byte pixel, byte color)
+    public static bool ContainsColorTrivial(byte pixel, byte color)
     {
         for (var i = 0; i < 4; i++, pixel >>= 2)
         {
@@ -37,5 +41,4 @@ public static class Question3
         }
         return false;
     }
-    */
 }
