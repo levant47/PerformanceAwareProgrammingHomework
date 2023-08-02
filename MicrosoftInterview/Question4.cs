f993fa7a88d2e764722c3dc8c3429747a6beed07
levant47 <levant.ba@gmail.com>
2023-08-03 01:22:42 +0300

    Better solution for question 4

--- MicrosoftInterview/Question4.cs
+++ MicrosoftInterview/Question4.cs
@@ -23,40 +23,18 @@ public static class Question4
     public static void OutlineCircle(int cX, int cY, int r, byte[] bitmap, int pitch)
     {
         // x^2 + y^2 = r^2 -> y^2 = r^2 - x^2 -> y = sqrt(r^2 - x^2)
-
-        // TODO: there's no need to do left + right side separately because only the quarter-circle is unique
-
-        // left side
         var nextY = 0;
-        for (var x = -r; x < 0; x++)
+        for (var x = -r; x <= 0; x++)
         {
             var y = nextY;
             nextY = (int)Math.Round(Math.Sqrt(r * r - (x + 1) * (x + 1)));
             do
             {
-                // fill top and bottom semicircle
+                // fill all 4 quarters
                 bitmap[(y + cY) * pitch + x + cX] = 1;
                 bitmap[(-y + cY) * pitch + x + cX] = 1;
-                y++; // more than one iteration if there is a gap between points
-            }
-            while (y < nextY);
-        }
-
-        // middle
-        bitmap[(cY + cY) * pitch + 0 + cX] = 1;
-        bitmap[(-cY + cY) * pitch + 0 + cX] = 1;
-
-        // right side
-        nextY = 0;
-        for (var x = r; x > 0; x--)
-        {
-            var y = nextY;
-            nextY = (int)Math.Round(Math.Sqrt(r * r - (x - 1) * (x - 1)));
-            do
-            {
-                // fill top and bottom semicircle
-                bitmap[(y + cY) * pitch + x + cX] = 1;
-                bitmap[(-y + cY) * pitch + x + cX] = 1;
+                bitmap[(y + cY) * pitch - x + cX] = 1;
+                bitmap[(-y + cY) * pitch - x + cX] = 1;
                 y++; // more than one iteration if there is a gap between points
             }
             while (y < nextY);
