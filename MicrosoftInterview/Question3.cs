ab16666cab5cf4bd4b3503d64cf64348261de6a5
levant47 <levant.ba@gmail.com>
2023-08-03 09:28:56 +0300

    Fix question 3

--- MicrosoftInterview/Question3.cs
+++ MicrosoftInterview/Question3.cs
@@ -18,16 +18,15 @@ public static class Question3
     // I don't think this version is really any faster than the loop one (assuming the compiler unrolls the loop in the loop version)
     public static bool ContainsColor(byte pixel, byte color)
     {
-        // TODO: this doesn't work!!!
-
         // pack the color into a byte with 4 pixels of the same color
         var color4 = color | (color << 2) | (color << 4) | (color << 6);
         // compute the difference between all 4 pairs of pixels
         var difference = pixel ^ color4;
         // if all four pixels in the difference have either their odd or their even bit set to 1, then the pixel did not contain our target color;
-        // so we are looking to see if there is at least one pixel with both its odd and even bits set to 0
-        var oddBits = 0b10101010; var evenBits = 0b01010101;
-        return (difference & oddBits) != oddBits && (difference & evenBits) != evenBits;
+        // so we are looking to see if there is at least one pixel with both its odd and even bits set to 0, and in order to do that, we perform a
+        // logical OR on all 4 pairs of bits which we do by processing odd bits as one argument of the OR and even bits as the other
+        var mask = 0b10101010;
+        return ((difference & mask) | ((difference << 1) & mask)) != mask;
     }
 
     public static bool ContainsColorTrivial(byte pixel, byte color)
